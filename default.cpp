72d6b08b85fd044f833ce6d3f0e906b18b185fd8
Koichiro Kanaya <abcs516tk1@gmail.com>
2021-01-02 18:32:34 +0900

    first commit

---
+++ default.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<int>>;
+
+template<class T>inline bool chmax(T &a, const T &b) { if (b > a) { a = b; return true; } return false; }
+template<class T>inline bool chmin(T &a, const T &b) { if (b < a) { a = b; return true; } return false; }
+
+constexpr int INF = 1001001001;
+constexpr long long LINF = 1001001001001001001;
+constexpr double EPS = 1e-10;
+constexpr double PI = M_PI;
+
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed << setprecision(20);
+
+    solve();
+
+    return 0;
+}
