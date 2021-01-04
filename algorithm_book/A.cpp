0076742a16476f5d40c3bd1df5f93e2ee09d26b6
Koichiro Kanaya <abcs516tk1@gmail.com>
2021-01-04 15:09:14 +0900

    5=3

--- algorithm_book/A.cpp
+++ algorithm_book/A.cpp
@@ -14,32 +14,28 @@ constexpr double PI = M_PI;
 
 
 void solve() {
-    int n, w;
-    cin >> n >> w;
+    int n;
+    cin >> n;
     vector<int>a(n);
+    int SUM = 0;
     for(int i=0;i<n;i++){
         cin >> a[i];
+        SUM += a[i];
     }
-    vector<vector<bool>>dp(n+1, vector<bool>(w+1, false));
-    for(int i=0;i<n+1;i++){
-        dp[i][0] = true;
-    }
+    vector<bool>dp(SUM+1, false);
+    dp[0] = true;
     for(int i=0;i<n;i++){
-        for(int j=0;j<w+1;j++){
-            if(j-a[i] >= 0 && dp[i][j-a[i]]){
-                dp[i+1][j] = true;
-            }
-            if(dp[i][j]){
-                dp[i+1][j] = true;
+        for(int j=dp.size()-1;j>=0;j--){
+            if(j-a[i] >= 0 && dp[j-a[i]]){
+                dp[j] = true;
             }
         }
     }
-    if(dp[n][w]){
-        cout << "Yes" << endl;
+    int ans = 0;
+    for(int i=0;i<dp.size();i++){
+        if(dp[i])ans++;
     }
-    else{
-        cout << "No" << endl;
-    }
+    cout << ans << endl;
 }
 
 int main() {
