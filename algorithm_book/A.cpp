38b83d9a239852fb29929ab9af81027006ea6efc
Koichiro Kanaya <abcs516tk1@gmail.com>
2021-01-04 20:02:13 +0900

    Re.7-3

--- algorithm_book/A.cpp
+++ algorithm_book/A.cpp
@@ -4,6 +4,8 @@ using namespace std;
 using ll = long long;
 using Graph = vector<vector<int>>;
 #define int long long
+#define fi first
+#define se second
 #define debug(x) cerr << #x << ": " << x << '\n' 
 
 template<class T>inline bool chmax(T &a, const T &b) { if (b > a) { a = b; return true; } return false; }
@@ -14,27 +16,33 @@ constexpr long long LINF = 1001001001001001001;
 constexpr double EPS = 1e-10;
 constexpr double PI = M_PI;
 
+bool cmp(pair<int,int>&a, pair<int,int>&b){
+    return a.se < b.se;
+}
+
 void solve() {
     int n;
     cin >> n;
-    vector<int>a(n), b(n);
+    vector<pair<int,int>>a(n);
     for(int i=0;i<n;i++){
-        cin >> a[i];
+        cin >> a[i].fi >> a[i].se;
     }
+    sort(a.begin(), a.end(), [](pair<int,int>a, pair<int,int>b){
+        return a.second < b.second;});
+    bool flag = true;
+    int tmp = 0;
     for(int i=0;i<n;i++){
-        cin >> b[i];
-    }
-    int tmp = -1;
-    int ans = 0;
-    vector<bool>vec(n,false);
-    for(int i=0;i<n;i++){
-        auto itr = lower_bound(a.begin(), a.end(), b[i]);
-        if(tmp < itr - a.begin()){
-            tmp = itr - a.begin();
-            ans++;
+        if(tmp+a[i].fi > a[i].se){
+            flag = false;
         }
+        tmp += a[i].fi;
     }
-    cout << ans << endl;
+    if(flag){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
 }
 
 signed main() {
