75ddd88b76d3f8fcc8a1c2a8f03a6c7c7742a934
Koichiro Kanaya <abcs516tk1@gmail.com>
2021-01-04 11:05:25 +0900

    4-5

--- algorithm_book/A.cpp
+++ algorithm_book/A.cpp
@@ -12,26 +12,45 @@ constexpr long long LINF = 1001001001001001001;
 constexpr double EPS = 1e-10;
 constexpr double PI = M_PI;
 
-vector<ll>vec;
+ll K;
+int ans = 0;
+map<ll, vector<bool>>mp;
 
-ll tribo(int n){
-    if(n <= 2){
-        if(vec[n] != -1){
-            return vec[n];
+void func(ll n) {
+    if(10*n + 3 <= K){
+        mp[10*n + 3] = {true, mp[n][1], mp[n][2]};
+        if(mp[10*n + 3][0] && mp[10*n + 3][1] && mp[10*n + 3][2]){
+            ans++;
         }
-        return vec[n] = max(0, n-1);
+        func(10*n+3);
     }
-    if(vec[n] != -1){
-        return vec[n];
+    if(10*n + 5 <= K){
+        mp[10*n + 5] = {mp[n][0], true, mp[n][2]};
+        if(mp[10*n + 5][0] && mp[10*n + 5][1] && mp[10*n + 5][2]){
+            ans++;
+        }
+        func(10*n+5);
     }
-    return vec[n] = tribo(n-1) + tribo(n-2) + tribo(n-3);
+    if(10*n + 7 <= K){
+        mp[10*n + 7] = {mp[n][0], mp[n][1], true};
+        if(mp[10*n + 7][0] && mp[10*n + 7][1] && mp[10*n + 7][2]){
+            ans++;
+        }
+        func(10*n+7);
+    }
+    else{
+        return;
+    }
 }
 
 void solve() {
-    int n;
-    cin >> n;
-    vec.assign(n+1,-1);
-    ll ans = tribo(n);
+    cin >> K;
+    mp[3] = {true, false, false};
+    func(3);
+    mp[5] = {false, true, false};
+    func(5);
+    mp[7] = {false, false, true};
+    func(7);
     cout << ans << endl;
 }
 
