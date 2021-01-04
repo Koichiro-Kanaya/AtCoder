f3799d0b352833b70fef0509f70c6ed80700bf9e
Koichiro Kanaya <abcs516tk1@gmail.com>
2021-01-04 18:20:10 +0900

    6-2

--- algorithm_book/A.cpp
+++ algorithm_book/A.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 using ll = long long;
 using Graph = vector<vector<int>>;
+#define int long long
+#define debug(x) cerr << #x << ": " << x << '\n' 
 
 template<class T>inline bool chmax(T &a, const T &b) { if (b > a) { a = b; return true; } return false; }
 template<class T>inline bool chmin(T &a, const T &b) { if (b < a) { a = b; return true; } return false; }
@@ -15,19 +17,29 @@ constexpr double PI = M_PI;
 void solve() {
     int n;
     cin >> n;
-    vector<int>a(n), b(n);
+    vector<int>a(n), b(n), c(n);
     for(int i=0;i<n;i++){
         cin >> a[i];
-        b[i] = a[i];
+    }
+    for(int i=0;i<n;i++){
+        cin >> b[i];
+    }
+    for(int i=0;i<n;i++){
+        cin >> c[i];
     }
     sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    sort(c.begin(),c.end());
+    int ans = 0;
     for(int i=0;i<n;i++){
-        auto ans = lower_bound(a.begin(), a.end(), b[i]);
-        cout << ans-b.begin() << endl;
+        auto it1 = lower_bound(c.begin(),c.end(), b[i]+1);
+        auto it2 = lower_bound(a.begin(), a.end(), b[i]);
+        ans += (n - (it1 - c.begin())) * (it2 - a.begin());
     }
+    cout << ans << endl;
 }
 
-int main() {
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout << fixed << setprecision(20);
