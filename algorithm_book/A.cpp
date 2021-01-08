df9802358c6076ece86495881346c5525632cdf5
Koichiro Kanaya <abcs516tk1@gmail.com>
2021-01-08 17:30:10 +0900

    12-2

--- algorithm_book/A.cpp
+++ algorithm_book/A.cpp
@@ -16,63 +16,24 @@ constexpr long long LINF = 1001001001001001001;
 constexpr double EPS = 1e-10;
 constexpr double PI = M_PI;
 
-struct UnionFind {
-    vector<int>par, siz;
-    UnionFind (int n) : par(n, -1), siz(n, 1) {};
-
-    // 自身がrootなら自分の番号を返す
-    int root(int a){
-        if(par[a] == -1) return a;
-        return par[a] = root(par[a]);
-    }
-
-    bool issame(int a, int b) {
-        return root(a) == root(b);
-    }
-
-    bool unite(int a, int b){
-        int x = root(a);
-        int y = root(b);
-        if(x == y) return false;
-
-        if(siz[x] < siz[y])swap(x, y);
-
-        par[y] = x;
-        siz[x] += siz[y];
-        return true;
-    }
-
-    int size(int x){
-        return siz[root(x)];
-    }
-};
-
 void solve() {
     int n,m;
     cin >> n >> m;
-    int ans = 0;
-    vector<pair<int, int>>a(m);
-    for(int i=0;i<m;i++){
-        int x,y;
-        cin >> x >> y;
-        x--,y--;
-        a[i].fi = x, a[i].se = y;
+    vector<pair<int,int>>a(n);
+    for(int i=0;i<n;i++){
+        cin >> a[i].fi >> a[i].se;
     }
-    for(int i=0;i<m;i++){
-        UnionFind uf(n);
-        for(int j=0;j<m;j++){
-            if(i==j)continue;
-            uf.unite(a[j].fi, a[j].se);
+    sort(a.begin(),a.end());
+    int ans = 0;
+    int cnt = 0;
+    for(int i=0;i<n;i++){
+        if(cnt+a[i].se >= m){
+            ans += (m-cnt) * a[i].fi;
+            break;
         }
-
-        int cnt = 0;
-        for(int j=0;j<n;j++){
-            if(uf.root(j) == j){
-                cnt++;
-            }
-        }
-        if(cnt > 1){
-            ans++;
+        else{
+            ans += a[i].se * a[i].fi;
+            cnt += a[i].se;
         }
     }
     cout << ans << endl;
